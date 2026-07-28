bf65f7feb30ce2ed8d787ecb413d904e52f26e68
Abdou Mandara <abdoumandara@gmail.com>
2026-07-28 15:34:44 +0100

    Mise en place du loader

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,15 @@ import AccountList from './components/AccountList'
 
 function App() {
   const [name, setName] = useState<string>('')
-  const [list_users, setList_users] = useState<GitHubUser[] | null>([]);
+  const [list_users, setList_users] = useState<GitHubUser[] | null>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true)
       const result = await GET(name)
       setList_users(result)
+      setIsLoading(false)
     }
     fetchData()
   }, [name])
@@ -19,7 +22,11 @@ function App() {
   return (
     <div className='w-screen h-full flex flex-col justify-center items-center p-6'>
       <SearchBar name={name} onNameChange={setName}/>
-      <AccountList users={list_users} />
+      {isLoading ? (
+        <span className="loading loading-spinner loading-xl"></span>
+      ):(
+        <AccountList users={list_users} />
+      )}
       <div>
         <p>{name}</p>
       </div>
