926b6ba1f200d46164bea8fdfb755e23c786f43a
Abdou Mandara <abdoumandara@gmail.com>
2026-07-30 13:24:47 +0100

    Redesign pour le responsive

--- src/components/AccountItem.tsx
+++ src/components/AccountItem.tsx
@@ -11,17 +11,21 @@ function AccountItem({user}: Props) {
         <div className="input py-2">{user.html_url}</div>
       </div>
       {/* Card ou on affiche les infos */}
-      <div className="card card-side bg-base-100 p-6 shadow-sm">
-        <figure className='h-80'>
+      <div className="card bg-base-100 p-6 shadow-sm flex flex-col md:card-side md:flex-row">
+        <figure className="w-full h-64 mb-4 md:w-auto md:h-80 md:mb-0">
           <img
+            className="h-full w-full object-cover rounded-xl"
             src={user.avatar_url}
-            alt={user.bio} />
+            alt={user.bio}
+          />
         </figure>
-        <div className="card-body">
-          <h2 className="card-title text-center">{user.name}</h2>
-          <p>{user.bio ? user.bio : user.name + " n'a pas de bio, c'est zarre-bi franchement :("}</p>
+        <div className="card-body flex flex-col h-full">
+          <div>
+            <h2 className="card-title text-left">{user.name}</h2>
+            <p>{user.bio ? user.bio : user.name + " n'a pas de bio, c'est zarre-bi franchement :("}</p>
+          </div>
           {/* Partie des stats */}
-          <div className="stats shadow">
+          <div className="stats stats-vertical md:stats-horizontal shadow w-full">
             <div className="stat">
               <div className="stat-figure text-secondary">
                 <svg
@@ -83,8 +87,8 @@ function AccountItem({user}: Props) {
             </div>
           </div>
           {/* Fin partie des stats */}
-          <div className="card-actions justify-end">
-            <a className="btn btn-primary" href={user.html_url} target='blank'>
+          <div className="card-actions mt-auto justify-center md:justify-end">
+            <a className="btn btn-primary w-full md:w-auto" href={user.html_url} target='blank'>
               <svg aria-label="GitHub logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="white" d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"></path></svg>
               Voir GitHub
             </a>
